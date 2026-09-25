80e9c7ed1c3893a357091a3ff0eaad3b2dfd988b
Leidy Sanchez <leysanchezb92@hotmail.com>
2026-09-24 19:43:14 -0500

    fix: replace stadia maps with openfreemap for dark tiles

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -9,8 +9,8 @@ const TILES = {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   },
   dark: {
-    url: 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
-    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    url: 'https://tiles.openfreemap.org/styles/dark/{z}/{x}/{y}.png',
+    attribution: '&copy; <a href="https://openfreemap.org">OpenFreeMap</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   },
 };
 
